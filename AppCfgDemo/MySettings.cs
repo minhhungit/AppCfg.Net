b2e3e317ffae1fef73d8205ff86966693fbc0e45
Hung Vo <it.minhhung@gmail.com>
2018-04-13 21:04:43 +0700

    better description

--- AppCfgDemo/MySettings.cs
+++ AppCfgDemo/MySettings.cs
@@ -12,8 +12,8 @@ namespace AppCfgDemo
             // register custom type parser
             MyAppCfg.TypeParserFactory.AddParser(new ListIntParser());
 
-            // by default, AppCfg will auto create [Parser] for json type (IJsonDataType) in runtime
-            // so, if you want to overwrite it by your parser (DemoParserWithRawBuilder) then you have to register it first
+            // by default, AppCfg will auto create [JsonParser] for json type (IJsonDataType) in runtime
+            // so, if you want to overwrite it by your parser [DemoParserWithRawBuilder] then you have to register it first
             MyAppCfg.TypeParserFactory.AddParser(new DemoParserWithRawBuilder<JsonPerson>());
 
             // setup json serializer settings
